17d174e2218311ebbe393cc7466886f4722ea647
François Poirotte <clicky@erebot.net>
2010-11-01 13:36:53 +0000

    Cleanup, correct licensing information.

--- packagexmlsetup.php
+++ packagexmlsetup.php
@@ -1,4 +1,8 @@
 <?php
+
+$package->license = 'GPL';
+$compatible->license = 'GPL';
+
 /**
  * Extra package.xml settings such as dependencies.
  * More information: http://pear.php.net/manual/en/pyrus.commands.make.php#pyrus.commands.make.packagexmlsetup
